3a642238c0a2c2d103d4bc8cbdfff9cebb827923
앵가리 <>
2022-12-05 18:47:00 +0900

    Refactoring: 객체 모듈화

--- main.js
+++ main.js
@@ -2,37 +2,7 @@ var http = require('http');
 var fs = require('fs'); // file system
 var url = require('url');
 var qs = require('querystring');
-
-var template = {
-  HTML: function(title, list, body, control) {
-    return `
-      <!doctype html>
-      <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-          </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ol>
-            ${list}
-          </ol>
-          ${control}
-          ${body}
-        </body>
-      </html>
-      `
-  },
-  list: function(filelist) {
-    var list = '';
-    var i = 0;
-    while(i < filelist.length) {
-      list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-      i = i + 1;
-    }
-    return list;
-  }
-}
+var template = require('./lib/template.js')
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
